5e0618f928a5a3ac484c6ad9132968fc1a073d8a
Nicole Bond <nicbond22@gmail.com>
2025-04-13 11:48:11 -0400

    Initial Commit

---
+++ BondAboutMeApp/BondAboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BondAboutMeAppApp.swift
+//  BondAboutMeApp
+//
+//  Created by Scholar on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct BondAboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
