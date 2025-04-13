d193f0bc1b52224345e0105858d9a3559f4dbba4
Nicole Bond <nicbond22@gmail.com>
2025-04-13 12:25:36 -0400

    Added in images and outline of content

--- BondAboutMeApp/ContentView.swift
+++ BondAboutMeApp/ContentView.swift
@@ -9,13 +9,89 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            
+//my picture and intro of me
+            VStack{
+                VStack{
+                    Image("MainPhoto")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(30)
+                    
+                    
+                    Text("Info about me")
+                    
+                    Text("emoji")
+                }
+                .padding()
+                .background(Rectangle()
+                .foregroundColor(Color(red: 255, green: 100, blue: 0)))
+                .cornerRadius(30)
+                .padding()
+                
+//pictures from Spring CAB weekends
+                HStack{
+                    
+//first CAB in SF
+                    VStack{
+                        Image("SFCAB")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(30)
+                        
+                        
+                        Text("Info about SF")
+                        
+                        Text("emoji")
+                    }
+                    .padding()
+                    .background(Rectangle()
+                    .foregroundColor(Color(red: 255, green: 100, blue: 0)))
+                    .cornerRadius(30)
+                    .padding()
+                    
+//second CAB in STL
+                    VStack{
+                        Image("STLCAB")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(30)
+                        
+                        
+                        Text("Info about STL")
+                        
+                        Text("emoji")
+                    }
+                    .padding()
+                    .background(Rectangle()
+                    .foregroundColor(Color(red: 255, green: 100, blue: 0)))
+                    .cornerRadius(30)
+                    .padding()
+                }
+                
+//info about Twix
+                VStack{
+                    Image("Twix")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(30)
+                    
+                    Text("Info about Twix")
+                    
+                    
+                    Text("emoji")
+                    
+                }
+                
+                .padding()
+                .background(Rectangle()
+                .foregroundColor(Color(red: 255, green: 100, blue: 0)))
+                .cornerRadius(30)
+                .padding()
+            }
         }
-        .padding()
+        
     }
 }
 
